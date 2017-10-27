1841778828ea331d042ae2d4df37e29713eb6032
dbolotov <dbolotov@live.com>
2017-10-27 01:35:24 -0700

    adding notes, empty scripts

---
+++ R/anode.R
@@ -0,0 +1,7 @@
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+anode <- function() {
+  print("Hello, world!")
+}

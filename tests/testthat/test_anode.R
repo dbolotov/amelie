ec3f817f65e63a6f5f0e4a8a965eebad258b6375
dbolotov <dbolotov@live.com>
2017-11-09 01:34:50 -0800

    added test skeletons

--- tests/testthat/test_anode.R
+++ tests/testthat/test_anode.R
@@ -1,5 +1,17 @@
 context("anode object")
 
-test_that("equality test passes", {
+test_that("fail when x is not matrix", {
+  expect_equal(10, 10)
+})
+
+test_that("fail when y is not matrix", {
+  expect_equal(10, 10)
+})
+
+test_that("fail when x is not numeric", {
+  expect_equal(10, 10)
+})
+
+test_that("fail when y is not numeric", {
   expect_equal(10, 10)
 })
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -8,7 +8,14 @@ test_that("f1 score is calculated correctly", {
   expect_equal(.f1_score(c(1,1,1,1,0),c(0,0,1,1,0)),2/3)
 })
 
-#epsilon optimization function
-test_that("asdf", {
+test_that("epsilon optimization works correctly", {
+  expect_equal(0,0)
+})
+
+test_that("univariate gaussian is calculated correctly", {
+  expect_equal(0,0)
+})
+
+test_that("data split works correctly", {
   expect_equal(0,0)
 })

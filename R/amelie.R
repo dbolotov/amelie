b0fe3db1f5659306f7d123b0c882c68823854fad
dbolotov <dbolotov@live.com>
2018-02-18 22:32:29 -0800

    added package-level doc

---
+++ R/amelie.R
@@ -0,0 +1,18 @@
+#' amelie: A package for anomaly ditection.
+#'
+#' Anomaly detection with maximum likelihood estimates and normal probability functions.
+#'
+#' @section Amelie functions:
+#' The package contains a function for running the anomaly detection algorithm.
+#'
+#' @section More information:
+#' \code{\link{ad}} documents the main \code{ad} function.
+#'
+#' For more details and examples, see the vignette by running the following:
+#'
+#' \code{vignette("Introduction")}
+#'
+#'
+#' @docType package
+#' @name amelie
+NULL
